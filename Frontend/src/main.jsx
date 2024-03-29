fc183f8a74914067a965a0b5024c4a69fbd876ff
dennis289 <edenismacharia7@gmail.com>
2024-03-29 07:24:43 +0300

    Fix file path typo and adjust build configuration

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -15,5 +15,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
      </SocketContextProvider>
     </AuthContextProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
